2ad8519639620da8e365e332589f51d80d5473db
gin <gindog@protonmail.ch>
2019-01-21 22:52:28 +0100

    Ajout du port au log startup

--- main.go
+++ main.go
@@ -16,18 +16,18 @@ import (
 
 
 func main() {
+	listen_port := config.GetListenPort()
+
 	allowedHeaders := handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "content-type",  "Accept"})
 	allowedMethods := handlers.AllowedMethods([]string{"GET", "HEAD", "POST"})
 	allowCredentials := handlers.AllowCredentials()
 
-
 	storage.Storage().AutoMigrate(&request.Request{})
-
 
 	router := routers.NewRouter()
 	negroniTool := negroni.Classic()
 	negroniTool.UseHandler(handlers.CORS(allowedHeaders, allowedMethods, allowCredentials)(router))
 
-	log.Println("Starting service...")
-	log.Fatal(http.ListenAndServe(":"+config.GetListenPort(), negroniTool))
+	log.Println("Starting service on port", listen_port)
+	log.Fatal(http.ListenAndServe(":"+listen_port, negroniTool))
 }
